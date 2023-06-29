49acb95b9bdee02e11917963d81e77472c5c51ce
Uraxii <accounts@brandonpaul.me>
2023-06-29 15:58:15 -0400

    spawn manager now uses events

--- Assets/Scripts/Spawning/SpawnManager.cs
+++ Assets/Scripts/Spawning/SpawnManager.cs
@@ -1,8 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class SpawnManagerSingleton : MonoBehaviour
 {
-    [SerializeField] SpawnTrigger[] linkedSpawns;
+    SpawnTrigger[] linkedSpawns;
+    public delegate void AnnounceTrigger(string label);
+    public static event AnnounceTrigger onAnnounceTrigger;
+
+    void Awake()
+    {
+        linkedSpawns = FindObjectsOfType<SpawnTrigger>();
+    }
+
+    public void RecieveTrigger(string label)
+    {
+        onAnnounceTrigger?.Invoke(label);
+    }
 }
