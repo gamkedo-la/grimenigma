ffd8c7da8692d5f1a6bf6d51074df4b1075a2a84
Uraxii <accounts@brandonpaul.me>
2023-07-01 16:54:31 -0400

    rail weapon logic

---
+++ Assets/Scripts/Player/Weapons/Rail.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour
+{
+    [SerializeField] float range, attackCoolDown;
+    [SerializeField] int damage;
+    [SerializeField] PlayerCameraControl pCamera;
+
+    bool shouldAttack = true;
+    RaycastHit attackHit;
+
+    public void Attack()
+    {
+        if (shouldAttack && Physics.Raycast(pCamera.transform.position, pCamera.transform.forward, out attackHit, range)){
+            //Debug.Log("Attack!");
+            //Debug.DrawRay(pCamera.transform.position, pCamera.transform.forward, Color.blue, 2f);
+            attackHit.transform.gameObject.GetComponent<HealthController>()?.Damage(damage);
+        }
+    }
+
+    IEnumerator ResetAttackCooldown()
+    {
+        shouldAttack = false;
+        yield return new WaitForSeconds(attackCoolDown);
+        shouldAttack = true;
+    }
+}
