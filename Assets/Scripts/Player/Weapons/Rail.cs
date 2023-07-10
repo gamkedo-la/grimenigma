eefc9ac44802832116bf0c535032fd87f3aa1261
Uraxii <uraxii@brandonpaul.me>
2023-07-10 15:27:17 -0400

    Added charge up to rail gun, also some animation code that doesn't do anything >:|

--- Assets/Scripts/Player/Weapons/Rail.cs
+++ Assets/Scripts/Player/Weapons/Rail.cs
@@ -4,24 +4,56 @@ using UnityEngine;
 
 public class Rail : MonoBehaviour
 {
-    [SerializeField] float range, attackCoolDown;
+    [SerializeField] float range, attackCoolDown, chargeTime, shakeIncrement;
     [SerializeField] int damage;
     [SerializeField] PlayerCameraControl pCamera;
 
+    bool charging;
     bool shouldAttack = true;
+
+    Vector3 shakePosition;
     RaycastHit attackHit;
 
-    public void Attack()
+    private void Update()
     {
-        if (shouldAttack && Physics.Raycast(pCamera.transform.position, pCamera.transform.forward, out attackHit, range)){
-            StartCoroutine(ResetAttackCooldown());
-            //Debug.Log("Attack!");
-            //Debug.DrawRay(pCamera.transform.position, pCamera.transform.forward, Color.blue, 2f);
-            attackHit.transform.gameObject.GetComponent<HealthController>()?.Damage(damage);
+        if(charging){
+            AnimationCharge();
         }
     }
 
-    IEnumerator ResetAttackCooldown()
+    public void Attack()
+    {
+        if (shouldAttack){
+            StartCoroutine(RunResetAttackCooldown());
+            StartCoroutine(RunChargedAttack());
+            //Debug.Log("Attack!");
+            //Debug.DrawRay(pCamera.transform.position, pCamera.transform.forward, Color.blue, 2f);
+        }
+    }
+
+    void AnimationCharge()
+    {
+        shakeIncrement = shakeIncrement*-50;
+        shakePosition = new Vector3(
+            transform.localPosition.x + shakeIncrement,
+            transform.localPosition.y + shakeIncrement,
+            transform.localPosition.z + shakeIncrement
+        );
+
+        transform.localPosition = shakePosition; //Vector3.Lerp(transform.localPosition, shakePosition, Time.deltaTime);
+    }
+
+    IEnumerator RunChargedAttack()
+    {
+        Debug.Log("Charging!");
+        charging = true;
+        yield return new WaitForSeconds(chargeTime);
+        charging = false;
+        Physics.Raycast(pCamera.transform.position, pCamera.transform.forward, out attackHit, range);
+        attackHit.transform.gameObject.GetComponent<HealthController>()?.Damage(damage);
+    }
+
+    IEnumerator RunResetAttackCooldown()
     {
         //Debug.Log("Started attack cooldown!");
         shouldAttack = false;
