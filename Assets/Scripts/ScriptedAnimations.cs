38971a10107fd11cfbb9777d1299c676e8b82e71
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-01 22:50:50 -0400

    class to contain general scripted animation logic

---
+++ Assets/Scripts/ScriptedAnimations.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptedAnimations : MonoBehaviour
+{
+
+    public void Twean(float speed, float x=0, float y=0, float z=0)
+    {
+        StartCoroutine(RunMoveTowards(speed, x, y, z));
+    }
+
+    public void Twean(float speed, Vector3 targetPosition)
+    {
+        StartCoroutine(RunMoveTowards(speed, targetPosition));
+    }
+
+    IEnumerator RunMoveTowards(float speed, float x=0, float y=0, float z=0)
+    {
+        Vector3 targetPosition = new Vector3(transform.position.x + x, transform.position.y + y, transform.position.z + z);
+        while(Vector3.Distance(transform.position, targetPosition) > 0.001f){
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+            //Debug.Log("New position: " + transform.position);
+            yield return null;
+        }
+    }
+
+        IEnumerator RunMoveTowards(float speed, Vector3 targetPosition)
+    {
+        while(Vector3.Distance(transform.position, targetPosition) > 0.001f){
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+            //Debug.Log("New position: " + transform.position);
+            yield return null;
+        }
+    }
+}
