6cdd085f576b66a3bc56d26000625c6d1e3496b8
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-01 23:00:14 -0400

    Created door opening/closing animation

--- Assets/Scripts/EncounterEvents/ListenerActions/OpenDoor.cs
+++ Assets/Scripts/EncounterEvents/ListenerActions/OpenDoor.cs
@@ -2,20 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ScriptedAnimations))]
 public class OpenDoor : MonoBehaviour
 {
-    [SerializeField] bool requiresKey;
+    [SerializeField] bool requiresKey = false;
     [SerializeField] string keyName;
+    [SerializeField] float distance;
+    [SerializeField] float speed;
+    [SerializeField] bool autoClose = true;
+    [SerializeField] float secondsBeforeAutoClose;
+    
 
+    Vector3 closedPosition;
+    Vector3 openPosition;
     bool isDoorClosed = true;
     EncounterListener listener;
     GameObject player;
+    ScriptedAnimations sa;
 
     void Awake()
     {
         listener = GetComponent<EncounterListener>();
-        listener.onEvent += CheckDoorOpen;
+        sa = GetComponent<ScriptedAnimations>();
         player = GameObject.Find("Player/Body");
+
+        closedPosition = transform.position;
+        openPosition = new Vector3(transform.position.x, transform.position.y+distance, transform.position.z);
+
+        listener.onEvent += CheckDoorOpen;
     }
 
     void CheckDoorOpen(string label)
@@ -23,9 +37,18 @@ public class OpenDoor : MonoBehaviour
         if(isDoorClosed && label == listener.label){
             if(!requiresKey || "Player has key check" != null){
                 Debug.Log("We are opening the door " + this.gameObject.name + "!");
-                transform.position = new Vector3(transform.position.x, transform.position.y+10, transform.position.z);
+                sa.Twean(speed, openPosition);
                 isDoorClosed = false;
+                StartCoroutine(RunCloseDoor());
             }
         }
+    }
+
+    IEnumerator RunCloseDoor()
+    {
+        yield return new WaitForSeconds(secondsBeforeAutoClose);
+        Debug.Log("We are closing the door " + this.gameObject.name + "!");
+        sa.Twean(speed, closedPosition);
+        isDoorClosed = true;
     }
 }
