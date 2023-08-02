23371eeedf440bbaccdb098b89616eb5392d3149
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-02 12:04:49 -0400

    added directional options to the door animation

--- Assets/Scripts/EncounterEvents/ListenerActions/OpenDoor.cs
+++ Assets/Scripts/EncounterEvents/ListenerActions/OpenDoor.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(ScriptedAnimations))]
 public class OpenDoor : MonoBehaviour
 {
+    [Header("Key Logic")]
     [SerializeField] bool requiresKey = false;
     [SerializeField] string keyName;
-    [SerializeField] float distance;
+    [Header("Animation")]
+    [SerializeField] float distanceX;
+    [SerializeField] float distanceY;
+    [SerializeField] float distanceZ;
     [SerializeField] float speed;
+    [Header("Closing")]
     [SerializeField] bool autoClose = true;
     [SerializeField] float secondsBeforeAutoClose;
     
@@ -27,7 +32,7 @@ public class OpenDoor : MonoBehaviour
         player = GameObject.Find("Player/Body");
 
         closedPosition = transform.position;
-        openPosition = new Vector3(transform.position.x, transform.position.y+distance, transform.position.z);
+        openPosition = new Vector3(transform.position.x+distanceX, transform.position.y+distanceY, transform.position.z+distanceZ);
 
         listener.onEvent += CheckDoorOpen;
     }
