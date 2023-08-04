6e3d3e3eaf6780bc5bd4949329b964441c5316c6
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-03 20:23:11 -0400

    Added rotation animation  to ScriptedAnimations

--- Assets/Scripts/ScriptedAnimations.cs
+++ Assets/Scripts/ScriptedAnimations.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class ScriptedAnimations : MonoBehaviour
 {
 
+    public void Rotate(float smoothing, float x=0, float y=0, float z=0)
+    {
+        StartCoroutine(RunRotate(x, y ,z));
+    }
+
     public void Twean(float speed, float x=0, float y=0, float z=0)
     {
         StartCoroutine(RunMoveTowards(speed, x, y, z));
@@ -26,13 +31,22 @@ public class ScriptedAnimations : MonoBehaviour
         }
     }
 
-        IEnumerator RunMoveTowards(float speed, Vector3 targetPosition)
+    IEnumerator RunMoveTowards(float speed, Vector3 targetPosition)
     {
         while(Vector3.Distance(transform.position, targetPosition) > 0.001f){
             float step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
             //Debug.Log("New position: " + transform.position);
             yield return null;
+        }
+    }
+
+    IEnumerator RunRotate(float x=0, float y=0, float z=0)
+    {
+        // while true (oooh scary)cx
+        while(true){
+            transform.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
         }
     }
 }
