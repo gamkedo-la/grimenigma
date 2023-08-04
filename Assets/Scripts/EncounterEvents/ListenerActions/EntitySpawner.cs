34360b8d99714d2ff2d3aceac3525485f2b6400d
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-03 21:51:47 -0400

    added previous selection check and isPlaying check

--- Assets/Scripts/EncounterEvents/ListenerActions/EntitySpawner.cs
+++ Assets/Scripts/EncounterEvents/ListenerActions/EntitySpawner.cs
@@ -62,6 +62,7 @@ public class EntitySpawnEditor : Editor
     {
         serializedObject.Update();
         EntitySpawner spawnNode = (EntitySpawner)target;
+        string lastSeclectedEnemy = spawnNode.entityToSpawn;
 
         base.OnInspectorGUI();
         GUILayout.BeginHorizontal();
@@ -75,7 +76,7 @@ public class EntitySpawnEditor : Editor
             //Debug.Log(selectedEnemyIndex);
             selectedEnemyIndex = EditorGUILayout.Popup(selectedEnemyIndex, enemyNames);
             spawnNode.entityToSpawn = enemyNames[selectedEnemyIndex];
-            EditorUtility.SetDirty(spawnNode);
+            if((lastSeclectedEnemy != spawnNode.entityToSpawn) && !Application.isPlaying) { EditorUtility.SetDirty(spawnNode); }
         }
         else{
             EditorGUILayout.Popup(0, new string[] {"No Objects Found!"});
