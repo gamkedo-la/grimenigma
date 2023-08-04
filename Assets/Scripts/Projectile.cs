b8b61b886e1ad33902aa6edfffc4daa53dda8cd4
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-03 21:02:52 -0400

    Added rotation animation to projectiles

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ScriptedAnimations))]
 public class Projectile : MonoBehaviour
 {
     [SerializeField] public ProjectileTypes type;
     [SerializeField] public int damage = 1;
     [SerializeField] public float speed = 1f;
     [SerializeField] public float range = 5000f;
+    [Header("Animaton")]
+    [SerializeField] ScriptedAnimations sa;
+    [SerializeField] float rotateX;
+    [SerializeField] float rotateY;
+    [SerializeField] float rotateZ;
 
     [HideInInspector] public string ownerTag;
 
     float travelDistance;
+    Vector3 direction;
 
     void OnEnable()
     {
         travelDistance = range;
+        direction = transform.forward;
+        sa.Rotate(rotateX, rotateY, rotateZ);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * speed * Time.deltaTime;
+        transform.position += direction * speed * Time.deltaTime;
         travelDistance -= speed * Time.deltaTime;
         if(travelDistance <= 0) { gameObject.SetActive(false); }
     }
