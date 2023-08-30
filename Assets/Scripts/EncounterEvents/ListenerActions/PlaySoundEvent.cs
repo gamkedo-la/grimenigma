d57683bd121e613ed936f073ce6692c2fe8417ae
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-08-30 00:27:00 -0400

    created listener event to play sounds

---
+++ Assets/Scripts/EncounterEvents/ListenerActions/PlaySoundEvent.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundEvent : MonoBehaviour
+{
+    [Header("Audio")]
+    [SerializeField] bool randomPitch;
+    [Range(0f, 0.3f)][SerializeField] float pitchRange = 0f;
+    [SerializeField] AudioSource sourceAudio;
+    [SerializeField] AudioClip soundClip;
+    EncounterListener listener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listener = GetComponent<EncounterListener>();
+        listener.onEvent += PlaySound;
+    }
+
+    void PlaySound(string label)
+    {
+        if(label == listener.label){
+            Debug.Log("Playing sound on " + gameObject.name);
+            if(randomPitch){ sourceAudio.pitch = Random.Range(-pitchRange, pitchRange); }
+            sourceAudio.PlayOneShot(soundClip);
+        }
+    }
+}
