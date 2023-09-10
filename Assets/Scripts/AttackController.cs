6be5fb094e55d870e8366add79a15f23b10d762a
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-09-10 19:44:02 -0400

    removed LineRenderer component requirement from ArtackController, is now created at Start if hitscan
    Having the LineRender as a child of GameObjects was messing up the transform gizmo in scene view.

--- Assets/Scripts/AttackController.cs
+++ Assets/Scripts/AttackController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
-[RequireComponent(typeof(LineRenderer))]
 public class AttackController : MonoBehaviour
 {
     [Header("Attack Settings")]
@@ -17,8 +17,8 @@ public class AttackController : MonoBehaviour
     [SerializeField] GameObject projectile;
     [Header("Tracer")]
     [SerializeField] bool shouldRenderTracer;
+    [SerializeField] GameObject tracer;
     [SerializeField] float tracerLifeSpan;
-    [SerializeField] LineRenderer tracerRenderer;
     //[SerializeField] float patternSteps = 0f;
     [Header("Ammo")]
     [SerializeField] int projectileAmmount = 1;
@@ -38,6 +38,7 @@ public class AttackController : MonoBehaviour
 
     Transform spawnOrigin;
     ProjectilePooler poolerSingleton;
+    LineRenderer tracerRenderer;
 
     bool shouldAttack = true;
     float targetRange;
@@ -85,7 +86,11 @@ public class AttackController : MonoBehaviour
     void Start()
     {
         poolerSingleton = FindObjectOfType<ProjectilePooler>().gameObject.GetComponent<ProjectilePooler>();
-        tracerRenderer = GetComponent<LineRenderer>();
+        if(attackType == AttackTypes.Hitscan){
+            tracerRenderer = this.AddComponent<LineRenderer>();
+            tracer = Instantiate(tracer, parent:this.gameObject.transform);
+            tracerRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        }
         if(!hasSourceOfTruth){ sourceOfTruth = this.gameObject; }
 
         spawnOrigin = this.gameObject.transform;        
@@ -94,6 +99,11 @@ public class AttackController : MonoBehaviour
     void Update()
     {
         if(hasSourceOfTruth){ transform.LookAt(sourceOfTruth.transform ); }
+    }
+
+    void OnDestroy()
+    {
+        if(attackType == AttackTypes.Hitscan){ Destroy(this.tracerRenderer.material); }
     }
 
     void DrawTracer()
@@ -172,6 +182,9 @@ public class AttackController : MonoBehaviour
     IEnumerator RunCreateAndDestroyTracer()
     {
         targetPosition = spawnOrigin.position + spawnOrigin.forward * range;
+    
+        tracerRenderer.startColor = Color.red;
+        tracerRenderer.endColor = Color.white;
         tracerRenderer.SetPosition(0, transform.position);
         tracerRenderer.SetPosition(1, targetPosition);
 
