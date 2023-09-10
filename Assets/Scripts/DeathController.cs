48f516f4738d426f43e2e82c8ec90dc07373adef
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-09-10 19:01:59 -0400

    moved resource drops into a dedicated function

--- Assets/Scripts/DeathController.cs
+++ Assets/Scripts/DeathController.cs
@@ -4,28 +4,15 @@ public class DeathController: MonoBehaviour
 {
     [Header("OnDeath")]
     [SerializeField] bool destroyOnDeath = true;
+    [SerializeField] bool dropItem;
+    [SerializeField] public GameObject[] itemDrops;
     [Header("Optional Settings")]
     [SerializeField] GameObject owner;
-
-    public GameObject[] itemDrops;
-    int randomItemDrop;
-    RaycastHit hit;
     GameObject thingToKill;
+
     public void HandleDeath()
     {
-        if(this.gameObject.tag == "enemy") {
-            randomItemDrop = Random.Range(0, itemDrops.Length);
-            if (Physics.Raycast(this.gameObject.transform.position + new Vector3(0, 4, 0), Vector3.down, out hit))
-            {
-                // Calculate the item drop position based on the ground hit point
-                Vector3 groundHitPoint = hit.point;
-
-                // Set the item drop position at a fixed height above the ground
-                Vector3 itemDropPosition = new Vector3(groundHitPoint.x, groundHitPoint.y + 1f, groundHitPoint.z);
-
-                Instantiate(itemDrops[randomItemDrop], itemDropPosition, this.gameObject.transform.rotation);
-            }
-        }
+        if(dropItem){ DropResource(); }
 
         if(destroyOnDeath){ Destroy(thingToKill); }
         else { thingToKill.SetActive(false); }
@@ -36,5 +23,22 @@ public class DeathController: MonoBehaviour
     void Start(){
         if(owner != null){ thingToKill = owner; }
         else{ thingToKill = this.gameObject; }
+    }
+
+    void DropResource()
+    {
+            int randomItemDrop = Random.Range(0, itemDrops.Length);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(this.gameObject.transform.position + new Vector3(0, 4, 0), Vector3.down, out hit))
+            {
+                // Calculate the item drop position based on the ground hit point
+                Vector3 groundHitPoint = hit.point;
+
+                // Set the item drop position at a fixed height above the ground
+                Vector3 itemDropPosition = new Vector3(groundHitPoint.x, groundHitPoint.y + 1f, groundHitPoint.z);
+
+                Instantiate(itemDrops[randomItemDrop], itemDropPosition, this.gameObject.transform.rotation);
+            }
     }
 }
