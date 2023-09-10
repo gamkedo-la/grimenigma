1c77dbab656aa9e920095d7ffed16176a3c3a8d3
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-09-10 18:51:59 -0400

    move thingTokillCheck into Start()

--- Assets/Scripts/DeathController.cs
+++ Assets/Scripts/DeathController.cs
@@ -25,12 +25,14 @@ public class DeathController: MonoBehaviour
             }
         }
 
-        if(owner != null){ thingToKill = owner; }
-        else{ thingToKill = this.gameObject; }
-
         if(shouldDestory){ Destroy(thingToKill); }
         else { thingToKill.SetActive(false); }
 
         Debug.Log("Entity " + this + " has died!");
+    }
+
+    void Start(){
+        if(owner != null){ thingToKill = owner; }
+        else{ thingToKill = this.gameObject; }
     }
 }
