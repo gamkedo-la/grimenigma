8787d583998722b6dbcd5cb646df516d0b05676f
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-09-24 19:54:48 -0400

    created SetMusicIntensity listener event

---
+++ Assets/Scripts/EncounterEvents/ListenerActions/SetMusicManagerIntensity.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetValue : MonoBehaviour
+{
+    [SerializeField] int value;
+
+    EncounterListener listener;
+    MusicManager musicManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        listener = GetComponent<EncounterListener>();
+        listener.onEvent += SetIntensity;
+
+        musicManager = GameObject.FindGameObjectWithTag("MusicManager").GetComponent<MusicManager>();
+    }
+
+    void SetIntensity(string label)
+    {
+        if(listener.label == label){ musicManager.intensity =  value; }
+    }
+}
