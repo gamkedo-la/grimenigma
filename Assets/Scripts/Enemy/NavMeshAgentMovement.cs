0d6ce84a198cbf9488dbbecc12e49e54537fe877
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-10-06 17:27:02 -0400

    updated navemesh movement position selection

--- Assets/Scripts/Enemy/NavMeshAgentMovement.cs
+++ Assets/Scripts/Enemy/NavMeshAgentMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine.AI;
 using UnityEngine;
 using Unity.AI.Navigation;
 using UnityEditor;
+using System.Transactions;
+using Unity.VisualScripting;
 
 public class NavMeshAgentMovement : MonoBehaviour
 {
@@ -12,8 +14,7 @@ public class NavMeshAgentMovement : MonoBehaviour
     [SerializeField] float minWaitTime, maxWaitTime;
 
     NavMeshAgent agent;
-    Vector3 spawnPosition;
-    Vector3 destination;
+    Vector3 spawnPosition, destination, lastKnownValidPath;
 
     bool isFindingPath = false;
 
@@ -42,44 +43,60 @@ public class NavMeshAgentMovement : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         spawnPosition = transform.position;
+        lastKnownValidPath = transform.position;
     }
 
     IEnumerator RunSetNewValidPosition(Vector3 position, float searchRadius, float maxDistance, float minHoldTime, float maxHoldTime)
     {
+        //Debug.Log("-COROUTINE START-");
+        
         isFindingPath = true;
-
-        NavMeshPath path = new NavMeshPath();
+        
         Vector3 targetPosition;
+        NavMeshPath path = new NavMeshPath();
+        NavMeshHit hit = new NavMeshHit();
 
-        bool searchingForValidPosition = true;
-        float yieldTime = 0.001f;
         float totalPathDistance = 0.0f;
         float holdTime = Random.Range(minHoldTime, maxHoldTime);
+        bool searchingForValidPosition = true;
+        bool targetOnNavMesh = false;
+        int allowedTries = 100;
+        int attmeptCount = 0;
 
-        do{ 
+        do{
+            //Debug.Log("Getting new position!");
             targetPosition = transform.position + Random.insideUnitSphere * searchRadius;
+            targetOnNavMesh = NavMesh.SamplePosition(targetPosition, out hit, 1f, NavMesh.AllAreas);
             NavMesh.CalculatePath(position, targetPosition, NavMesh.AllAreas, path);
-            if(path.status == NavMeshPathStatus.PathComplete){
-
+            if(targetOnNavMesh && path.status == NavMeshPathStatus.PathComplete){
+                //Debug.Log("Got valid path!");
                 for(int i = 1; i < path.corners.Length; i++){
                     totalPathDistance += Vector3.Distance(path.corners[i-1], path.corners[i]);
                 }
                 
-                if(totalPathDistance <= maxDistance){ searchingForValidPosition = false; }
+                if(totalPathDistance <= maxDistance){
+                    //Debug.Log("Path is within range!");
+                    lastKnownValidPath = targetPosition;
+                    searchingForValidPosition = false;
+                    break;
+                }
             }
-            else{
-                searchRadius = searchRadius * 0.98f; 
-                holdTime -= yieldTime;
-                yield return new WaitForSeconds(yieldTime);
+
+            attmeptCount ++;
+            if(attmeptCount > allowedTries){
+                //Debug.Log("Too many attempts. Breaking!");
+                targetPosition = lastKnownValidPath;
+                break;
             }
-            //Debug.Log(totalPathDistance);
-        }while(searchingForValidPosition);
+            yield return new WaitForSeconds(0.0001f);
+        }while(searchingForValidPosition || attmeptCount < allowedTries);
 
         yield return new WaitForSeconds(holdTime);
 
         agent.SetDestination(targetPosition);
 
         isFindingPath = false;
+        //Debug.Log("-COROUTINE END-");
     }
 
     IEnumerator RunMaintainDisance(Vector3 position, float distance)
