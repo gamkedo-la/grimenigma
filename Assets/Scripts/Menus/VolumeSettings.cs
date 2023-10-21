8592bbd825d1a782df27e50c3d61e5f995327375
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-10-21 14:15:20 -0400

    volume settings script

--- Assets/Scripts/Menus/VolumeSettings.cs
+++ Assets/Scripts/Menus/VolumeSettings.cs
@@ -1,9 +1,35 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Audio;
 
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] AudioMixer mixer;
+    [SerializeField] ObjectToJSON jsonHandler;
+
+    public void AppyVolumeSettings()
+    {
+        float master, music, fx, weapons;
+        mixer.GetFloat("MasterVolume", out master);
+        mixer.GetFloat("MusicVolume", out music);
+        mixer.GetFloat("FXVolume", out fx);
+        mixer.GetFloat("WeaponsVolume", out weapons);
+
+        PlayerPrefs.SetFloat("MasterVolume", master);
+        PlayerPrefs.SetFloat("MusicVolume", music);
+        PlayerPrefs.SetFloat("FXVolume", fx);
+        PlayerPrefs.SetFloat("WeaponsVolume", weapons);
+
+        Debug.Log(PlayerPrefs.GetFloat("MasterVolume"));
+    }
+
+    public void SetVolumeSettings()
+    {
+       mixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+       mixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
+       mixer.SetFloat("FXVolume", PlayerPrefs.GetFloat("FXVolume"));
+       mixer.SetFloat("WeaponsVolume", PlayerPrefs.GetFloat("WeaponsVolume"));
+    }
 
     public void SetMasterVolume(float val)
     {
