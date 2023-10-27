88c5e2e26941602e6f6e7fd5441c160c7c7a18ac
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-10-26 22:54:01 -0400

    created file for default player prefs values

---
+++ Assets/Scripts/PlayerPrefsDefault.cs
@@ -0,0 +1,8 @@
+public static class PlayerPrefsDefault
+{
+    public static float defaultGamepadHorizontalSensativity = 500;
+    public static float defaultGamepadVerticleSensativity = 500;
+    public static float defaultMouseHorizontalSensativity = 10;
+    public static float defaultMouseVerticleSensativity = 10;
+    public static float defaultFov = 100;
+}
