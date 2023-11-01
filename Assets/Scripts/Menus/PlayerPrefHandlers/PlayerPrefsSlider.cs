eda2f20f8ef9233a350c1c76cce89383e2e6b9c8
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-11-01 18:07:13 -0400

    PlayerPrefSlider now updates value in update

--- Assets/Scripts/Menus/PlayerPrefHandlers/PlayerPrefsSlider.cs
+++ Assets/Scripts/Menus/PlayerPrefHandlers/PlayerPrefsSlider.cs
@@ -1,12 +1,20 @@
+using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerPrefsSlider : PlayerPrefsUI
 {
+    Slider slider;
+
     void Start()
     {
-        Slider slider = this.GetComponent<Slider>();
+        slider = this.GetComponent<Slider>();
         slider.onValueChanged.AddListener (delegate {
             OnValueChange(slider.value);
         });  
+    }
+
+    void Update()
+    {
+        if(slider.value != (float)prefferenceHandler.GetValue()){ slider.value = (float)prefferenceHandler.GetValue(); }
     }
 }
