0c1e3dca1cb50d4027a3b857b212c51ccd04913d
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-11-02 11:26:31 -0400

    moved MusicTrackData class into dedicated file

--- Assets/Scripts/MusicManager/MusicManager.cs
+++ Assets/Scripts/MusicManager/MusicManager.cs
@@ -2,13 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public class MusicTrackData
-{
-    [Range(0, 10)]public int intensity;
-    public AudioClip audio;
-}
-
 public class MusicManager : MonoBehaviour
 {
     [SerializeField] List<MusicTrackData> music;
