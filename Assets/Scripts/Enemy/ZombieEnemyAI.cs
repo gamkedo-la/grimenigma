7baf212beaefc2ed64b5b3a72d1a415bdeed8113
Uraxii <60625500+Uraxii@users.noreply.github.com>
2023-11-15 17:51:25 -0500

    Hitscan enemies now fire projectiles, Migrated Hitscan enemies to new AI system

---
+++ Assets/Scripts/Enemy/ZombieEnemyAI.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ZombieEnemyAI : EnemyBaseAI
+{
+    public override void HandleAttack()
+    {
+        StartCoroutine(RunAttack(target.position, Random.Range(1, maxAttacks+1), weapon.cooldown));
+    }
+    public override void HandleAlerted()
+    {
+        isPerformingAction = false;
+        agentMove.ClearPath();
+        HandleChase();
+    }
+    public override void HandleChase()
+    {
+        agentMove.MaintainDistacne(target.position, maintainDistanceFromTarget);
+        if(IsTargetWithinAttackRange()){ state = AIState.attack; }
+        else{ state = AIState.chase; }
+    }
+    public override void OnBeginAttack()
+    {
+        PlaySoundFX(alertSound);
+    }
+}
